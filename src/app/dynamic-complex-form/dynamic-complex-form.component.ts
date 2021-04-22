163eef22479a62f0b7f5da868273b0dac0d39231
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-04-22 20:06:17 +0800

    Migrated to dynamic-complex-form component.

--- src/app/dynamic-complex-form/dynamic-complex-form.component.ts
+++ src/app/dynamic-complex-form/dynamic-complex-form.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComplexFormComponent implements OnInit {
 
+  controls: {component?: Promise<any>|null, value: any, inputs?: any}[] = [];
+
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.controls = [
+      { component: import('./../controls/currency/currency.component').then(m => m.CurrencyComponent), value: 1 },
+      { component: import('./../controls/number/number.component').then(m => m.NumberComponent), value: 1  },
+      { component: import('./../controls/select/select.component').then(m => m.SelectComponent), value: 1, inputs: {options: ['Mumbai'] }},
+      { component: import('./../controls/slider/slider.component').then(m => m.SliderComponent), value: 1  },
+    ];
   }
 
 }
