3cb82237920b679f48953440cd096245b55401be
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-04-22 19:57:17 +0800

    Added route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DynamicComponentLoadingComponent } from './dynamic-component-loading/dynamic-component-loading.component';
 import { DynamicComponentLoadingScamComponent } from './dynamic-component-loading-scam/dynamic-component-loading-scam.component';
+import { DynamicComplexFormComponent } from './dynamic-complex-form/dynamic-complex-form.component';
+import { DynamicSimpleFormComponent } from './dynamic-simple-form/dynamic-simple-form.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'dynamic-component-loading', component: DynamicComponentLoadingComponent },
   {path: 'dynamic-component-loading-scam', component: DynamicComponentLoadingScamComponent },
+  {path: 'dynamic-complex-form', component: DynamicComplexFormComponent },
+  {path: 'dynamic-simple-form', component: DynamicSimpleFormComponent },
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
