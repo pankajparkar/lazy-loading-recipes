c03f21a350d957cd7d51a03457c6be06c4ae90ec
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-04-22 19:59:30 +0800

    1. Changed routes on home page 2. Added dynamic-simple-form component code.

--- src/app/dynamic-simple-form/dynamic-simple-form.component.ts
+++ src/app/dynamic-simple-form/dynamic-simple-form.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicSimpleFormComponent implements OnInit {
 
+  controls: {component?: Promise<any>|null}[] = [
+    { component: import('./../controls/currency/currency.component').then(c => c.CurrencyComponent) },
+    { component: import('./../controls/number/number.component').then(c => c.NumberComponent)  },
+    { component: import('./../controls/select/select.component').then(c => c.SelectComponent)  },
+    { component: import('./../controls/slider/slider.component').then(c => c.SliderComponent)  },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
